6e1471c7f506d9c6d736ff88c2f6aa10b753c58a
Marcel Schoen <marcelschoen@yahoo.com>
2014-09-28 22:59:21 +0000

    Working on unit test support

---
+++ gamepad4j-api/src/test/java/org/gamepad4j/ControllerListenerAdapterTest.java
@@ -0,0 +1,16 @@
+/*
+ * @Copyright: Marcel Schoen, Switzerland, 2014, All Rights Reserved.
+ */
+
+package org.gamepad4j;
+
+/**
+ * Tests the controller listener adapter.
+ *
+ * @author Marcel Schoen
+ * @version $Revision: $
+ */
+public class ControllerListenerAdapterTest {
+
+	
+}
