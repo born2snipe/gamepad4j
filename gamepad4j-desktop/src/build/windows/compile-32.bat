54f1646a7a3b33fee408ed832e586954de3e5575
marcelschoen <marcelschoen@yahoo.com>
2014-10-05 22:59:39 +0200

    Fixed Windows 32 bit compile

--- gamepad4j-desktop/src/build/windows/compile-32.bat
+++ gamepad4j-desktop/src/build/windows/compile-32.bat
@@ -6,7 +6,6 @@ SET INCLUDE=..\..\..\src\main\h\shared
 SET JDK_INCLUDE="C:\Program Files\Java\jdk1.7.0_67\include"
 SET JDK_INCLUDE_WINDOWS="C:\Program Files\Java\jdk1.7.0_67\include\win32"
 SET TARGET_32=..\..\..\%BIN_DIR%\windows-32
-SET TARGET_64=..\..\..\%BIN_DIR%\windows-64
 
 RMDIR /S /Q %TARGET_32%
 MD %TARGET_32%\tmp
@@ -15,11 +14,3 @@ gcc -m32 -Wall -pthread -c -I %INCLUDE% -I %JDK_INCLUDE% -I %JDK_INCLUDE_WINDOWS
 gcc -m32 -Wall -pthread -c -I %INCLUDE% -I %JDK_INCLUDE% -I %JDK_INCLUDE_WINDOWS% %SRC_WINDOWS%\Gamepad_windows.c -o %TARGET_32%\tmp\Gamepad_windows.o
 rem gcc -m32 -shared -Wall -o %TARGET_32%\libgamepad4j.dll %TARGET_32%\tmp\*.o -lwinmm 
 gcc -o %TARGET_32%\libgamepad4j.dll -s -m32 -shared %TARGET_32%\tmp\*.o -Wl,--subsystem,windows -Wl,--add-stdcall-alias  -lwinmm 
-
-
-RMDIR /S /Q %TARGET_64%
-MD %TARGET_64%\tmp
-gcc -m64 -Wall -pthread -c -I %INCLUDE% -I %JDK_INCLUDE% -I %JDK_INCLUDE_WINDOWS% %SRC_SHARED%\Gamepad_private.c -o %TARGET_64%\tmp\Gamepad_private.o
-gcc -m64 -Wall -pthread -c -I %INCLUDE% -I %JDK_INCLUDE% -I %JDK_INCLUDE_WINDOWS% %SRC_SHARED%\GamepadJniWrapper.c -o %TARGET_64%\tmp\GamepadJniWrapper.o
-gcc -m64 -Wall -pthread -c -I %INCLUDE% -I %JDK_INCLUDE% -I %JDK_INCLUDE_WINDOWS% %SRC_WINDOWS%\Gamepad_windows.c -o %TARGET_64%\tmp\Gamepad_windows.o
-gcc -o %TARGET_64%\libgamepad4j.dll -s -m64 -shared %TARGET_64%\tmp\*.o -Wl,--subsystem,windows -Wl,--add-stdcall-alias  -lwinmm 
